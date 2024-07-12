a3975d0a77250e5e54f7158da2dcdf60fe669e54
smonger <mongersteven@gmail.com>
2024-07-12 12:00:06 +0930

    Update run.py

--- data/run.py
+++ data/run.py
@@ -61,28 +61,29 @@ score_types = {'ds_ag': hl.tfloat64, 'ds_al': hl.tfloat64, 'ds_dg': hl.tfloat64,
                'sai_loss125': hl.tfloat64, 'sai_loss150': hl.tfloat64, 'sai_1xgb': hl.tfloat64, 'ssm_1e': hl.tfloat64, 'ssm_1amne': hl.tfloat64, 'ssm_1amdi': hl.tfloat64,
                'ssm_1amnsu': hl.tfloat64, 'sai_2xgb': hl.tfloat64, 'ssm_2e': hl.tfloat64, 'ssm_2amne': hl.tfloat64,  'ssm_2amdi': hl.tfloat64, 'ssm_2amnsu': hl.tfloat64, 'start': hl.tint32}
 
-data = hl.import_table(input_file, delimiter="\t", missing="", types=score_types)
-
-#data = data.transmute(ds_ag=float64(data.ds_ag),
-#                      ds_al=float64(data.ds_al),
-#                      ds_dg=float64(data.ds_dg),
-#                      ds_dl=float64(data.ds_dl),
-#                      sai_sum=float64(data.sai_sum),
-#                      sai_max=float64(data.sai_max),
-#                      sai_loss125=float64(data.sai_loss125),
-#                      sai_loss150=float64(data.sai_loss150),
-#                      sai_1xgb=float64(data.sai_1xgb),
-#                      sai_2xgb=float64(data.sai_2xgb),
-#                      ssm_1e=float64(data.ssm_1e),
-#                      ssm_1amne=float64(data.ssm_1amne),
-#                      ssm_1amdi=float64(data.ssm_1amdi),
-#                      ssm_1amnsu=float64(data.ssm_1amnsu),
-#                      ssm_2e=float64(data.ssm_2e),
-#                      ssm_2amne=float64(data.ssm_2amne),
-#                      ssm_2amdi=float64(data.ssm_2amdi),
-#                      ssm_2amnsu=float64(data.ssm_2amnsu))
-
+data = hl.import_table(input_file, delimiter="\t", missing="")#, types=score_types)
 data = data.filter(data.start != "start")
+
+data = data.transmute(ds_ag=float64(data.ds_ag),
+                      ds_al=float64(data.ds_al),
+                      ds_dg=float64(data.ds_dg),
+                      ds_dl=float64(data.ds_dl),
+                      sai_sum=float64(data.sai_sum),
+                      sai_max=float64(data.sai_max),
+                      sai_loss125=float64(data.sai_loss125),
+                      sai_loss150=float64(data.sai_loss150),
+                      sai_1xgb=float64(data.sai_1xgb),
+                      sai_2xgb=float64(data.sai_2xgb),
+                      ssm_1e=float64(data.ssm_1e),
+                      ssm_1amne=float64(data.ssm_1amne),
+                      ssm_1amdi=float64(data.ssm_1amdi),
+                      ssm_1amnsu=float64(data.ssm_1amnsu),
+                      ssm_2e=float64(data.ssm_2e),
+                      ssm_2amne=float64(data.ssm_2amne),
+                      ssm_2amdi=float64(data.ssm_2amdi),
+                      ssm_2amnsu=float64(data.ssm_2amnsu))
+                      start=int32(data.start))
+
 
 # Index the variants for which scores are available to match the keys of Hail tables
 data = data.key_by(locus=hl.locus(data.chr, data.start, reference_genome="GRCh38"), alleles=[data.ref, data.alt])
