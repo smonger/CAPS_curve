a1d14b57c431918556d59ec46e061a3f474d7dcf
smonger <mongersteven@gmail.com>
2024-07-23 23:00:33 +0930

    Update run_missense.py

--- data/run_missense.py
+++ data/run_missense.py
@@ -1,1 +1,95 @@
 
+import hail as hl
+from preprocessing import preprocessing
+from hail.expr.functions import int32, float64
+
+#### Specify Exome or Genome here ####
+exome_or_genome = "exome"
+
+#### Specify whether t generate by_csq files. Only required when refitting CAPS
+create_by_csq_file = False
+
+#### Constants and file paths ####
+xy_exome_total = 363624
+xx_exome_total = 367323
+xy_genome_total = 37237
+xx_genome_total = 38942
+
+gnomad_gs_path = "gs://gcp-public-data--gnomad/"
+mikgud_gs_path = "gs://vccri-mikgud-uscentral1/"
+
+exomes_ht_path = gnomad_gs_path + "release/4.1/ht/exomes/gnomad.exomes.v4.1.sites.ht"
+genomes_ht_path = gnomad_gs_path + "release/4.1/ht/genomes/gnomad.genomes.v4.1.sites.ht/"
+context_ht_path = gnomad_gs_path + "resources/context/grch38_context_vep_annotated.v105.ht"
+coverage_exomes_ht_path = gnomad_gs_path + "release/4.0/coverage/exomes/gnomad.exomes.v4.0.coverage.ht/"
+coverage_genomes_ht_path = gnomad_gs_path + "release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht/"
+lof_metrics_by_gene = "https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1.1/constraint/gnomad.v2.1.1.lof_metrics.by_gene.txt.bgz"
+
+input_splicing_exomes = mikgud_gs_path + "hg38_gnomad_v4_exome_splicing_scores_27052024.bgz"
+input_splicing_genomes = mikgud_gs_path + "hg38_gnomad_v4_genome_splicing_scores_27052024.bgz"
+output_splicing_exomes = mikgud_gs_path + "hg38_gnomad_v4_exome_splicing_scores_27052024_data_output.bgz"
+output_splicing_genomes = mikgud_gs_path + "hg38_gnomad_v4_genome_splicing_scores_27052024_data_output.bgz"
+by_csq_file_genome = mikgud_gs_path + "gnomad_v4.1.0_genomes_by_csq.tsv"
+by_csq_file_exome = mikgud_gs_path + "gnomad_v4.1.0_exomes_by_csq.tsv"
+
+if exome_or_genome == "genome":
+  ht_path = genomes_ht_path
+  coverage_ht_path = coverage_genomes_ht_path
+  input_file = input_splicing_genomes
+  output_file = output_splicing_genomes
+  xy_total = xy_genome_total
+  xx_total = xx_genome_total
+  by_csq_file = by_csq_file_genome
+elif exome_or_genome == "exome":
+  ht_path = exomes_ht_path
+  coverage_ht_path = coverage_exomes_ht_path
+  input_file = input_splicing_exomes
+  output_file = output_splicing_exomes
+  xy_total = xy_exome_total
+  xx_total = xx_exome_total
+  by_csq_file = by_csq_file_exome
+
+#### RUN ####
+
+gnomad = preprocessing(ht_path, context_ht_path, coverage_ht_path, {"female": xx_total, "male": xy_total})
+
+if create_by_csq_file:
+  gnomad.group_by("context", "ref", "alt", "methylation_level", "worst_csq", "coverage").aggregate(
+    variant_count = hl.agg.count(), singleton_count=hl.agg.count_where(gnomad.freq[0].AC == 1)).export(by_csq_file)
+
+score_types = {'ds_ag': hl.tfloat64, 'ds_al': hl.tfloat64, 'ds_dg': hl.tfloat64, 'ds_dl': hl.tfloat64, 'sai_sum': hl.tfloat64, 'sai_max': hl.tfloat64,
+               'sai_loss125': hl.tfloat64, 'sai_loss150': hl.tfloat64, 'sai_1xgb': hl.tfloat64, 'ssm_1e': hl.tfloat64, 'ssm_1amne': hl.tfloat64, 'ssm_1amdi': hl.tfloat64,
+               'ssm_1amnsu': hl.tfloat64, 'sai_2xgb': hl.tfloat64, 'ssm_2e': hl.tfloat64, 'ssm_2amne': hl.tfloat64,  'ssm_2amdi': hl.tfloat64, 'ssm_2amnsu': hl.tfloat64, 'start': hl.tint32}
+
+data = hl.import_table(input_file, delimiter="\t", missing="")#, types=score_types)
+data = data.filter(data.start != "start")
+
+data = data.transmute(alphamissense=float64(data.alphamissense),
+                      revel=float64(data.revel),
+                      primateai3d=float64(data.primateai3d),
+                      start=int32(data.start))
+
+# Index the variants for which scores are available to match the keys of Hail tables
+data = data.key_by(locus=hl.locus(data.chr, data.start, reference_genome="GRCh38"), alleles=[data.ref, data.alt])
+
+data = data[gnomad.key]
+gnomad = gnomad.annotate(            
+            revel=data.revel,
+            alphamissense=data.alphamissense,
+            primateai3d=data.primateai3d)
+
+gnomad = gnomad.annotate(AC=gnomad.freq[0].AC)
+gnomad = gnomad.filter(gnomad.worst_csq!="stop_gained")
+gnomad.select(
+            "context",
+            "ref",
+            "alt",
+            "methylation_level",
+            "worst_csq",
+            "protein_coding",
+            "coverage",
+            "AC",
+            "revel",
+            "alphamissense",
+            "primateai3d").export(output_file)
+
